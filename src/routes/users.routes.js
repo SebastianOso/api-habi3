bb5a0ff3c9d1d95bc53014889f11661985a1fc6f
SebastianOso <sebastian.oso03@gmail.com>
2025-09-12 01:07:03 -0600

    endpoint de login

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { getUsers } = require("../controllers/users.controller");
+const { getLogin } = require("../controllers/users.controller");
 
 // GET /api/users
 router.get("/", getUsers);
+router.get("/login/:id", getLogin);
 
 module.exports = router;
