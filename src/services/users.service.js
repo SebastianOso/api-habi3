774dda80cbab81ed41c6bde2e40c5059786728f8
SebastianOso <sebastian.oso03@gmail.com>
2025-09-12 00:51:02 -0600

    dar todos los usuarios

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,7 +1,7 @@
 const db = require("../../database");
 
 const getAllUsers = async () => {
-  const [rows] = await db.execute("SELECT * FROM usuarios");
+  const [rows] = await db.execute("SELECT * FROM user");
   return rows;
 };
 
